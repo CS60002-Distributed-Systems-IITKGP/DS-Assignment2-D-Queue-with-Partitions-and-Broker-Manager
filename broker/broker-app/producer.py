97cfc40f21b888aab10dc2c9970de0ee55238833
Sahil Mahapatra <sahilm231199@gmail.com>
2023-03-06 16:57:19 +0530

    structuring consumer endpoint broker manager

--- broker/broker-app/producer.py
+++ broker/broker-app/producer.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends, status, HTTPException
-from sqlalchemy.orm import Session
-from typing import List
-from core import database
-from models import Message, TopicPartition
-from pydantic import BaseModel
+# from fastapi import APIRouter, Depends, status, HTTPException
+# from sqlalchemy.orm import Session
+# from typing import List
+# from core import database
+# from models import Message, TopicPartition
+# from pydantic import BaseModel
 
 
 # get_db = database.get_db
